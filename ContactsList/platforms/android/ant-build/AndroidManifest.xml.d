ea5565af0f0f5d8b1768d01b638fab00f72a74c8
Béni Gál <glbni@yahoo.com>
2014-09-19 11:58:06 +0300

    Deleted the built Android folder.

--- ContactsList/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-c:\Users\5tr0ngKn1ghT\OneDrive\NGMS\phonegap\ContactsList\platforms\android\ant-build\AndroidManifest.xml : \
-c:\Users\5tr0ngKn1ghT\OneDrive\NGMS\phonegap\ContactsList\platforms\android\AndroidManifest.xml \
